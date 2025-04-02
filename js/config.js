875d34e60811f64ab21f9f1f516aa0a70694606f
iganama <160270819+iganama@users.noreply.github.com>
2025-04-02 14:42:55 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_oKG12MnP0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '71t3piv6qo555r6vn0pp6rgml6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5p0l7ses32aan3mp40ibo6nov4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
